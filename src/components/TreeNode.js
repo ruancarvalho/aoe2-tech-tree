c2d50f2f04eb78db034b47e9433ff1048954182d
Ruan Carvalho <ruancarvalho@gmail.com>
2019-11-18 17:04:54 -0400

    feat: items connectors using css

--- src/components/TreeNode.js
+++ src/components/TreeNode.js
@@ -4,13 +4,19 @@ import Button from './Button';
 const TreeNode = (props) => {
   const [isExpanded, toggleExpanded] = useState(true);
 
+  let classes = 'children';
+
+  if (React.Children.count(props.children) <= 1) {
+    classes = 'child'
+  }
+
   return (
     <ul className={props.type}>
       <li className="node">
         <Button type={props.type} name={props.name} onClick={() => toggleExpanded(!isExpanded)}/>
       </li>
       {isExpanded &&
-        <li className="children">
+        <li className={classes}>
           {props.children}
         </li>
       }
--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
 function Wrapper(props) {
+  let classes = 'children';
+  
+  if (React.Children.count(props.children) <= 1) {
+    classes = 'child'
+  }
+
   return (
     <ul className="wrapper">
       <li className="node blank" />
-      <li className="children">
+      <li className={classes}>
         { props.children }
       </li>
     </ul>
